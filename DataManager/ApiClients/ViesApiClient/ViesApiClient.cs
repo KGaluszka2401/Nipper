fd14eaac985f9297bbe7faeb2cfe146b502bd41b
Ahyr2401 <konradgaluszka33@gmail.com>
2025-01-06 13:01:52 +0100

    creates api client for validating eu nips

---
+++ DataManager/ApiClients/ViesApiClient/ViesApiClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+using Nipper.DataManager.ApiClients.ViesApiClient.Models;
+using Nipper.DataManager.Models;
+
+namespace Nipper.DataManager.ApClients.ViesApiClient;
+
+internal class ViesApiClient
+{
+    private static readonly HttpClient client = new();
+    private const string baseUrl = "https://ec.europa.eu/taxation_customs/vies/rest-api/ms/";
+
+    /// <summary>
+    /// Send a GET request to EU VIES service to validate a vat eu number.
+    /// </summary>
+    /// <param name="countryCode"></param>
+    /// <param name="nip"></param>
+    /// <returns>
+    /// <strong>ViesResponse</strong> object when vies service returns information about nip number or a 
+    /// <strong>null</strong> on a failed request.
+    /// </returns>
+    public async Task<IViesResponse> CheckNipAsync(string countryCode, string nip)
+    {
+        var uri = new Uri(baseUrl + countryCode + $"/vat/" + nip);
+        var response = await client.GetAsync(uri);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return new ViesExceptionResponse()
+            {
+                ErrorMesssage = $"Zapytanie nie powiodło się, serwer zwrócił kod {response.StatusCode}"
+            };
+        }
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        var viesResponse = JsonSerializer.Deserialize<ViesEntityResponse>(responseBody);
+
+        if (viesResponse == null)
+        {
+            return new ViesExceptionResponse()
+            {
+                ErrorMesssage = $"Nie powiodła się serializacja odpowiedzi serwera"
+            };
+        }
+
+        return viesResponse;
+    }
+}

43d4c91d1d156567434b786c04bf7373a82aeafa
Ahyr2401 <konradgaluszka33@gmail.com>
2025-01-06 18:19:01 +0100

    changes nip validator structure and adds vies vat number handling

--- DataManager/Utilities/NipValidator.cs
+++ DataManager/Utilities/NipValidator.cs
@@ -1,5 +1,7 @@
-﻿using Nipper.DataManager.ApClients.WlApiClient;
+﻿using Nipper.DataManager.ApClients.ViesApiClient;
 using Nipper.DataManager.ApClients.WlApiClient.Models;
+using Nipper.DataManager.ApiClients.ViesApiClient.Models;
+using Nipper.DataManager.ApiClients.WlApiClient;
 using Nipper.DataManager.Models;
 
 namespace Nipper.DataManager.Utilities;
@@ -7,6 +9,7 @@ namespace Nipper.DataManager.Utilities;
 public class NipValidator
 {
     private static readonly WlApiClient wlApiClient = new();
+    private static readonly ViesApiClient viesApiClient = new();
     public async IAsyncEnumerable<CompanyInfo> CheckNipsAsync(string[] nips)
     {
         int requestsSentBeforeBreak = 0;
@@ -23,17 +26,6 @@ public class NipValidator
                 continue;
             }
 
-            if (char.IsLetter(nip[0]) && char.IsLetter(nip[1]))
-            {
-                yield return new()
-                {
-                    Nip = nip,
-                    ErrorMesage = null,
-                    CompanyName = "Chuj wie czy istnieje, eu, potem dodam api calla"
-                };
-                continue;
-            }
-
             requestsSentBeforeBreak += 1;
             if (requestsSentBeforeBreak >= 20)
             {
@@ -41,32 +33,67 @@ public class NipValidator
                 await Task.Delay(3000);
             }
 
-            var response = await wlApiClient.CheckNipAsync(nip);
             CompanyInfo companyInfo = new()
             {
                 Nip = nip,
                 ErrorMesage = null,
                 CompanyName = null
             };
-            switch (response)
+
+            if (char.IsLetter(nip[0]) && char.IsLetter(nip[1]))
             {
-                case WlEntityResponse entityResponse:
-                    if (entityResponse.Result.Subject == null)
-                    {
-                        companyInfo.ErrorMesage = "Nie istnieje firma o podanym nipie";
-                        break;
-                    }
-                    companyInfo.CompanyName = entityResponse.Result.Subject.Name;
-                    break;
-                case WlExceptionResponse exceptionResponse:
-                    companyInfo.ErrorMesage =
-                        $"Wystąpił błąd {exceptionResponse.Code}: {exceptionResponse.Message}";
-                    break;
-                default:
-                    companyInfo.ErrorMesage = "Niezidentyfikowana akcja";
-                    break;
+                yield return await ValidateViesNipAsync(nip, companyInfo);
+                continue;
             }
-            yield return companyInfo;
+
+            yield return await ValidateEuNipAsync(nip, companyInfo);
         }
+    }
+
+    private static async Task<CompanyInfo> ValidateEuNipAsync(string nip, CompanyInfo companyInfo)
+    {
+        var wlResponse = await wlApiClient.CheckNipAsync(nip);
+        switch (wlResponse)
+        {
+            case WlEntityResponse entityResponse:
+                if (entityResponse == null || entityResponse.result == null || entityResponse.result.subject == null)
+                {
+                    companyInfo.ErrorMesage = "Nie istnieje firma o podanym nipie";
+                    break;
+                }
+                companyInfo.CompanyName = entityResponse.result.subject.name;
+                break;
+            case WlExceptionResponse exceptionResponse:
+                companyInfo.ErrorMesage =
+                    $"Wystąpił błąd {exceptionResponse.code}: {exceptionResponse.message}";
+                break;
+            default:
+                companyInfo.ErrorMesage = "Wystąpił niezidentyfikowany błąd";
+                break;
+        }
+        return companyInfo;
+    }
+
+    private static async Task<CompanyInfo> ValidateViesNipAsync(string nip, CompanyInfo companyInfo)
+    {
+        var viesResponse = await viesApiClient.CheckNipAsync(nip[..2], nip);
+        switch (viesResponse)
+        {
+            case ViesEntityResponse entityResponse:
+                if (!entityResponse.isVald)
+                {
+                    companyInfo.ErrorMesage = "Nie istnieje firma o podanym nipie";
+                    break;
+                }
+                companyInfo.CompanyName = entityResponse.name;
+                break;
+            case ViesExceptionResponse entityResponse:
+                companyInfo.ErrorMesage = entityResponse.ErrorMesssage;
+                break;
+            default:
+                companyInfo.ErrorMesage = "Wystąpił niezidentyfikowany błąd";
+                break;
+        }
+        return companyInfo;
     }
 }

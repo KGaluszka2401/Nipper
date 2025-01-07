6e28bf37a1333ad7bae899ae2702b4f3b7790478
Ahyr2401 <konradgaluszka33@gmail.com>
2025-01-07 17:35:44 +0100

    removes old using

--- DataManager/Utilities/XlsCreator.cs
+++ DataManager/Utilities/XlsCreator.cs
@@ -4,7 +4,6 @@ namespace Nipper.DataManager.Utilities;
 
 public static class XlsCreator
 {
-    private static readonly SettingsManager settingsManager = new();
     public static void CreateXlsxFile(Dictionary<string, string> nipResultDict, string savePath)
     {
         using var workbook = new XLWorkbook();

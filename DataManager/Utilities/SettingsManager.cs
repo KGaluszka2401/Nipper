05d9f88a625c5c6ed7cef2e031c8828596c0dbe2
Ahyr2401 <konradgaluszka33@gmail.com>
2025-01-07 18:18:18 +0100

    adds handling of deleted settings file

--- DataManager/Utilities/SettingsManager.cs
+++ DataManager/Utilities/SettingsManager.cs
@@ -28,8 +28,21 @@ public class SettingsManager
 
     private Settings GetSetting()
     {
+        if (!File.Exists(cfgPath))
+        {
+            Settings defaultSettings = new()
+            {
+                XlsOutputFolder = ""
+            };
+
+            string json = JsonSerializer.Serialize(defaultSettings, new JsonSerializerOptions() { WriteIndented = true });
+            File.WriteAllText(cfgPath, json);
+            return defaultSettings;
+        }
+
+
         string jsonString = File.ReadAllText(cfgPath);
         var cfg = JsonSerializer.Deserialize<Settings>(jsonString);
-        return cfg ?? throw new Exception("Missing config file");
+        return cfg ?? throw new("Nastąpił błąd odczytu pliku");
     }
 }
